a2a6fb32a661ef3c8191e5fcea461b68e0cd539e
Sarah <sarah.brunmayr@htl-wels.at>
2021-01-07 12:53:23 +0100

    UML CarDatabase implemented

--- src/model/CarDatabase.java
+++ src/model/CarDatabase.java
@@ -1,6 +1,16 @@
 package model;
 
+import java.util.HashMap;
 
 public class CarDatabase{
-    //hauptklasse
+
+    HashMap<String,Vehicle> db = new HashMap<>();
+
+    public CarDatabase(HashMap<String, Vehicle> db) {
+        this.db = db;
+    }
+
+    public Vehicle search(String licencePlate, boolean exact){
+
+    }
 }
