8845492f47ab44956dcf75938f6d66694114c05e
Sarah <sarah.brunmayr@htl-wels.at>
2021-01-07 12:51:33 +0100

    class Vehicle implemented

--- src/model/CarDatabase.java
+++ src/model/CarDatabase.java
@@ -1,17 +1,6 @@
 package model;
 
-import java.util.HashMap;
 
 public class CarDatabase{
     //hauptklasse
-
-    HashMap<String,Vehicle> db = new HashMap<>();
-
-    public CarDatabase(HashMap<String, Vehicle> db) {
-        this.db = db;
-    }
-
-    public Vehicle search(String licencePlate, boolean exact){
-
-    }
 }
